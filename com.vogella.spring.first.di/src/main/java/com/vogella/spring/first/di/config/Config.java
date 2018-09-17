fdc34e6a047095b6bc1b5e28cf6c37b82ac730a3
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-09-17 17:08:35 -0300

    6. Exercise - Spring DI Bean Config

--- com.vogella.spring.first.di/src/main/java/com/vogella/spring/first/di/config/Config.java
+++ com.vogella.spring.first.di/src/main/java/com/vogella/spring/first/di/config/Config.java
@@ -1,10 +1,26 @@
 package com.vogella.spring.first.di.config;
 
+import java.util.Date;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.vogella.spring.first.di.ITodo;
+import com.vogella.spring.first.di.Todo;
 
 @Configuration
 @ComponentScan(basePackages = { "com.vogella.spring.first.di" })
 public class Config {
+
+	@Bean
+	@Primary
+	public ITodo getTodo() {
+		ITodo todo = new Todo(0, "Spring DI");
+		todo.setDone(false);
+		todo.setDueDate(new Date());
+		return todo;
+	}
 
 }
