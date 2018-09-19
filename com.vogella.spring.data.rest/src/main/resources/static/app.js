bbc3afb6faf121f060d8eb31a811a6cf49d10b3f
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-09-19 15:52:11 -0300

    14. Exercise - Using rest.js

--- com.vogella.spring.data.rest/src/main/resources/static/app.js
+++ com.vogella.spring.data.rest/src/main/resources/static/app.js
@@ -1,1 +1,9 @@
 document.write("Hello first JavaScript sample!");
+
+const client = require('./restclient'); 
+client({path: '/api/tasks/1'}).then(response => { 
+    console.log('response: ', response);
+    document.write("response: " + response + " summary: " + response.summary);
+}, error => { 
+    document.write("error: " + error);
+});
