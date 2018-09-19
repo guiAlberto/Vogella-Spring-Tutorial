05065ec88b89bd8068e9267977a449516597934a
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-09-19 15:56:52 -0300

    15. Exercise - Creating a UI frontend with react

--- com.vogella.spring.data.rest/src/main/resources/static/app.js
+++ com.vogella.spring.data.rest/src/main/resources/static/app.js
@@ -1,9 +1,58 @@
-document.write("Hello first JavaScript sample!");
+'use strict';
 
-const client = require('./restclient'); 
-client({path: '/api/tasks/1'}).then(response => { 
-    console.log('response: ', response);
-    document.write("response: " + response + " summary: " + response.summary);
-}, error => { 
-    document.write("error: " + error);
-});
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./restclient');
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {tasks: []};
+    }
+
+    componentDidMount() {
+        client({method: 'GET', path: '/api/tasks'}).done(response => {
+            this.setState({tasks: response.entity._embedded.tasks});
+        });
+    }
+
+    render() {
+        return (
+            <TaskList tasks={this.state.tasks}/>
+        )
+    }
+}
+
+class TaskList extends React.Component{
+    render() {
+        var tasks = this.props.tasks.map(task =>
+            <Task key={task._links.self.href} task={task}/>
+        );
+        return (
+            <table>
+                <tr>
+                    <th>Summary</th>
+                    <th>Description</th>
+                </tr>
+                {tasks}
+            </table>
+        )
+    }
+}
+
+class Task extends React.Component{
+    render() {
+        return (
+            <tr>
+                <td>{this.props.task.summary}</td>
+                <td>{this.props.task.description}</td>
+            </tr>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+)
